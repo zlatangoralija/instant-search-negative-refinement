0f957fc195eb5f7801c018f081e870044d006e3c
zlatangoralija <zlatan@omnitask.ba>
2023-02-20 15:33:46 +0100

    add classes to filter elements

--- src/lib/negativeRefinementListRenderer.ts
+++ src/lib/negativeRefinementListRenderer.ts
@@ -15,65 +15,69 @@ export const createNegativeRefinementListRenderer: NegativeRefinementListRendere
       ? document.querySelector(container)!
       : container;
 
-  const list = document.createElement('ol');
+    const list = document.createElement('ul');
 
-  return {
-    /*
-     * The render function passed to the connector
-     * This function is called when we need to render the widget.
-     * The render appends when:
-     * - the widget is added to InstantSearch
-     * - we receive new results from Algolia
-     */
-    render(renderOptions, isFirstRender) {
-      /*
-       * `renderOptions` contains all options passed by the connector to the renderer, it contains everything needed for the rendering of the component
-       * See `renderOptions` documentation https://www.algolia.com/doc/
-       */
-      const { items, refine } = renderOptions;
-
-      console.log(renderOptions)
-
-      if (isFirstRender) {
+    return {
         /*
-         * When the widget is rendered for the first time `isFirstRender` is set to `true`
-         * This is when we will create everything that must be reused between renders (containers, event listeners, etc.)
+         * The render function passed to the connector
+         * This function is called when we need to render the widget.
+         * The render appends when:
+         * - the widget is added to InstantSearch
+         * - we receive new results from Algolia
          */
+        render(renderOptions, isFirstRender) {
+            /*
+             * `renderOptions` contains all options passed by the connector to the renderer, it contains everything needed for the rendering of the component
+             * See `renderOptions` documentation https://www.algolia.com/doc/
+             */
+            const { items, refine } = renderOptions;
 
-        containerNode.appendChild(list);
-      }
+            if (isFirstRender) {
+                /*
+                 * When the widget is rendered for the first time `isFirstRender` is set to `true`
+                 * This is when we will create everything that must be reused between renders (containers, event listeners, etc.)
+                 */
 
-      list.innerText = '';
+                containerNode.appendChild(list);
+            }
 
-      /*
-       * Update the widget using the data from `renderOptions`
-       */
-      items.forEach((item) => {
-        const li = document.createElement('li');
-        const label = document.createElement('label');
-        const input = document.createElement('input');
-        input.type = "checkbox"
-        input.checked = item.isExcluded;
+            list.innerText = '';
+            list.classList.add('ais-RefinementList-list')
 
-        input.addEventListener('change', (e) => {
-  refine(item.name)
-        })
+            /*
+             * Update the widget using the data from `renderOptions`
+             */
+            items.forEach((item) => {
+                const li = document.createElement('li');
+                const label = document.createElement('label');
+                const input = document.createElement('input');
 
-        const text = document.createTextNode(item.name);
+                li.classList.add('ais-RefinementList-item');
+                label.classList.add('ais-RefinementList-label');
+                input.classList.add('ais-RefinementList-checkbox');
 
-        li.appendChild(label)
-        label.appendChild(input)
-        label.appendChild(text)
-        list.appendChild(li);
-      });
-    },
-    /*
-     * The dispose function passed to the connector
-     * This function is called when the widget is removed from InstantSearch.
-     * It must be used to remove any changes made by the render function (DOM changes, global event listeners, etc.)
-     */
-    dispose() {
-      containerNode.removeChild(list);
-    },
-  };
+                input.type = "checkbox"
+                input.checked = item.isExcluded;
+
+                input.addEventListener('change', (e) => {
+                    refine(item.name)
+                })
+
+                const text = document.createTextNode(item.name);
+
+                li.appendChild(label)
+                label.appendChild(input)
+                label.appendChild(text)
+                list.appendChild(li);
+            });
+        },
+        /*
+         * The dispose function passed to the connector
+         * This function is called when the widget is removed from InstantSearch.
+         * It must be used to remove any changes made by the render function (DOM changes, global event listeners, etc.)
+         */
+        dispose() {
+            containerNode.removeChild(list);
+        },
+    };
 };
